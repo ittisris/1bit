ea29a3c72bd585cb0ab73e79690ab335f01192f3
Itti Srisumalai <i2ktel@hotmail.com>
2024-06-20 23:26:09 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -7,13 +7,37 @@ control.on_event(EventBusSource.MICROBIT_ID_IO_P8,
     EventBusValue.MICROBIT_PIN_EVT_RISE,
     on_microbit_id_io_p8_pin_evt_rise)
 
+def on_button_pressed_b():
+    pins.digital_write_pin(DigitalPin.P1, 1)
+    basic.pause(500)
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    basic.pause(500)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_microbit_id_button_b_pin_evt_rise():
+    led.plot(0, 0)
+    basic.pause(100)
+    led.unplot(0, 0)
+control.on_event(EventBusSource.MICROBIT_ID_BUTTON_B,
+    EventBusValue.MICROBIT_PIN_EVT_RISE,
+    on_microbit_id_button_b_pin_evt_rise)
+
+def on_microbit_id_io_p12_evt():
+    rrLoop()
+control.on_event(EventBusSource.MICROBIT_ID_IO_P12,
+    EventBusValue.MICROBIT_EVT_ANY,
+    on_microbit_id_io_p12_evt)
+
+def rrLoop():
+    input_port[0] = pins.digital_read_pin(DigitalPin.P12)
+
 def on_microbit_id_io_p8_pin_evt_fall():
     global pc
     if pins.digital_read_pin(DigitalPin.P1) == 0:
         if pins.digital_read_pin(DigitalPin.P2) == 0:
             readData()
         pc = pc + 1
-        if pc == len(rom):
+        if pc == len(opcode):
             pc = 0
 control.on_event(EventBusSource.MICROBIT_ID_IO_P8,
     EventBusValue.MICROBIT_PIN_EVT_FALL,
@@ -32,27 +56,28 @@ control.on_event(EventBusSource.MICROBIT_ID_IO_P1,
 
 def sendInstruc():
     global instruction
-    instruction = rom[pc]
-    pins.digital_write_pin(DigitalPin.P10, instruction % 2)
-    instruction = Math.round(instruction / 2)
-    pins.digital_write_pin(DigitalPin.P11, instruction % 2)
-    instruction = Math.round(instruction / 2)
-    pins.digital_write_pin(DigitalPin.P12, instruction % 2)
-    instruction = Math.round(instruction / 2)
+    instruction = opcode[pc]
     pins.digital_write_pin(DigitalPin.P13, instruction % 2)
+    instruction = Math.round(instruction / 2)
+    pins.digital_write_pin(DigitalPin.P14, instruction % 2)
+    instruction = Math.round(instruction / 2)
+    pins.digital_write_pin(DigitalPin.P15, instruction % 2)
+    instruction = Math.round(instruction / 2)
+    pins.digital_write_pin(DigitalPin.P16, instruction % 2)
 def reset():
     global pc
     pc = 0
     sendInstruc()
+    rrLoop()
     readData()
 instruction = 0
 pc = 0
 output_port: List[number] = []
 input_port: List[number] = []
 ioaddress: List[number] = []
-rom: List[number] = []
-rom = [0, 1]
-ioaddress = [0, 1]
+opcode: List[number] = []
+opcode = [3, 1]
+ioaddress = [0, 0]
 data_in = pins.digital_read_pin(DigitalPin.P0)
 input_port = [0, 0, 0]
 output_port = [0, 0, 0]
--- main.ts
+++ main.ts
@@ -1,52 +1,58 @@
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P8, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-        if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-            writeData()
-        }
-        sendInstruc()
-    }
-})
 control.onEvent(EventBusSource.MICROBIT_ID_IO_P12, EventBusValue.MICROBIT_EVT_ANY, function () {
     rrLoop()
 })
 function rrLoop () {
     input_port[0] = pins.digitalReadPin(DigitalPin.P12)
 }
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P8, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-        if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-            readData()
-        }
-        pc = pc + 1
-        if (pc == opcode.length) {
-            pc = 0
-        }
-    }
+input.onButtonPressed(Button.A, function () {
+    reset()
 })
+function clkPosEdge () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        writeData()
+    }
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pc = pc + 1
+    if (pc == opcode.length) {
+        pc = 0
+    }
+    basic.pause(500)
+}
+function clkNegEdge () {
+    sendInstruc()
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    basic.pause(500)
+}
 function writeData () {
     output_port[ioaddress[pc]] = pins.digitalReadPin(DigitalPin.P0)
 }
 function readData () {
     pins.digitalWritePin(DigitalPin.P0, input_port[ioaddress[pc]])
 }
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P1, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
-    reset()
+input.onButtonPressed(Button.B, function () {
+    clkNegEdge()
+    clkPosEdge()
 })
 function sendInstruc () {
     instruction = opcode[pc]
+    basic.showNumber(instruction)
     pins.digitalWritePin(DigitalPin.P13, instruction % 2)
-    instruction = Math.round(instruction / 2)
+    instruction = Math.floor(instruction / 2)
     pins.digitalWritePin(DigitalPin.P14, instruction % 2)
-    instruction = Math.round(instruction / 2)
+    instruction = Math.floor(instruction / 2)
     pins.digitalWritePin(DigitalPin.P15, instruction % 2)
-    instruction = Math.round(instruction / 2)
+    instruction = Math.floor(instruction / 2)
     pins.digitalWritePin(DigitalPin.P16, instruction % 2)
+    readData()
 }
 function reset () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
     pc = 0
-    sendInstruc()
-    rrLoop()
-    readData()
+    basic.showString("R")
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P1, 0)
 }
 let instruction = 0
 let pc = 0
@@ -54,8 +60,28 @@ let output_port: number[] = []
 let input_port: number[] = []
 let ioaddress: number[] = []
 let opcode: number[] = []
-opcode = [3, 1]
-ioaddress = [0, 0]
-let data_in = pins.digitalReadPin(DigitalPin.P0)
+led.setBrightness(50)
+opcode = [
+1,
+2,
+3,
+4,
+5
+]
+ioaddress = [
+0,
+1,
+0,
+1,
+0
+]
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+let tmp = pins.digitalReadPin(DigitalPin.P0)
 input_port = [0, 0, 0]
 output_port = [0, 0, 0]
+reset()
+basic.forever(function () {
+    basic.pause(100)
+})
